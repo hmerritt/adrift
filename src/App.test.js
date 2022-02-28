ddae6fc2f20f5e3db3ae82498bbc4133dd9ad0bb
hmerritt <harry@mrrtt.me>
2022-02-28 20:11:22 +0000

    tabs over spaces

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-import App from './App';
+import App from "./App";
 
-test('renders app', () => {
-    render(<App />);
-    // const linkElement = screen.getByText(/learn react/i);
-    // expect(linkElement).toBeInTheDocument();
+test("renders app", () => {
+	render(<App />);
+	// const linkElement = screen.getByText(/learn react/i);
+	// expect(linkElement).toBeInTheDocument();
 });
