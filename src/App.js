eb61f4119ce8b6a142c55f2370d16034954f5b25
hmerritt <harry@mrrtt.me>
2022-03-01 20:37:12 +0000

    edit example app to use useInterval

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import "./App.css";
+import { useState } from "react";
+import { useInterval } from "hooks/useInterval";
 
 function App() {
+	const [count, setCount] = useState("0");
+
+	useInterval(() => {
+		setCount((parseFloat(count) + parseFloat(0.1)).toFixed(2));
+	}, 100);
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<div className="flex-center" style={{ height: "100vh" }}>
+				<h1 style={{ fontSize: "3rem", textAlign: "center" }}>
+					{count}
+					<br />
+					<small>useInterval 100ms</small>
+				</h1>
+			</div>
 		</div>
 	);
 }
