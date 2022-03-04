969f70c4894cf281f217ec40d8fbc2d3747ddd3e
hmerritt <harry@mrrtt.me>
2022-03-04 21:01:20 +0000

    refactor: index to export all hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 
+import { useInterval } from "hooks";
 import { countIncrement } from "store/actions";
-import { useInterval } from "hooks/useInterval";
 
 function App() {
 	const dispatch = useDispatch();
