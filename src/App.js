8ea4d5c7a4fee951c0e487962d6ae2548a5372cc
hmerritt <harry@mrrtt.me>
2022-03-04 21:51:06 +0000

    feat: use useDebouncedCallback in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import { useInterval } from "hooks";
 import { countIncrement } from "store/actions";
+import { useDebouncedCallback, useInterval } from "hooks";
 
 function App() {
 	const dispatch = useDispatch();
 	const count = useSelector((state) => state.count.current);
 
+	// Debounce callback
+	const debounce = useDebouncedCallback(
+		() => dispatch(countIncrement(0.1)),
+		1000,
+		{ maxWait: 1000 }
+	);
+
 	useInterval(() => {
-		dispatch(countIncrement(0.1));
+		debounce();
 	}, 100);
 
 	return (
@@ -17,7 +24,10 @@ function App() {
 				<h1 style={{ fontSize: "3rem", textAlign: "center" }}>
 					{count}
 					<br />
+					<br />
 					<small>useInterval 100ms</small>
+					<br />
+					<small>useDebouncedCallback 1000ms</small>
 				</h1>
 			</div>
 		</div>
