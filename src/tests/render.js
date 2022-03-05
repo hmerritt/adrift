0a90054298c62b5390113b40159a2b5e1788b7cf
hmerritt <harry@mrrtt.me>
2022-03-05 23:25:29 +0000

    render routes in tests

--- src/tests/render.js
+++ src/tests/render.js
@@ -1,16 +1,24 @@
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { render as jestRender } from "@testing-library/react";
 
 import store from "store";
 
-export const render = (ui) => {
+export const render = (ui, route = "") => {
 	const Wrapper = ({ children }) => {
 		return (
 			<Provider store={store}>
-				<Router>{children}</Router>
+				<MemoryRouter initialEntries={[route]}>{children}</MemoryRouter>
 			</Provider>
 		);
+	};
+
+	return jestRender(ui, { wrapper: Wrapper });
+};
+
+export const renderBasic = (ui) => {
+	const Wrapper = ({ children }) => {
+		return <Provider store={store}>{children}</Provider>;
 	};
 
 	return jestRender(ui, { wrapper: Wrapper });
