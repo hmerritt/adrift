9af5e9e95deb54fb3b3d5d5405f4e20060809679
hmerritt <harry@mrrtt.me>
2022-03-06 15:45:34 +0000

    stream stdout of bootstrap command

--- bootstap-start.js
+++ bootstap-start.js
@@ -1,5 +1,6 @@
 const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const exec = require("child_process").exec;
+const execAwait = util.promisify(exec);
 
 const packageJSON = require("./package.json");
 
@@ -13,32 +14,66 @@ async function bootstrap() {
 		const appVersion = packageJSON?.version;
 
 		// Set ENV array to inject, key/value
-		const env = [];
-		env.push(["REACT_APP_VERSION", appVersion]);
-		env.push(["REACT_APP_GIT_COMMIT", gitCommitHash]);
+		const env = [
+			["REACT_APP_VERSION", appVersion],
+			["REACT_APP_GIT_COMMIT", gitCommitHash],
+		];
 
 		// Build ENV string
-		let envString = "";
-		env.forEach((item, index) => {
-			if (index > 0) envString += ` `;
-			envString += `${item[0]}=${item[1]}`;
-		});
-
-		console.log("Injecting the following ENV :", envString);
+		const envString = buildENV(env);
 
 		// Run react-scripts command
-		await run(`npx cross-env ${envString} react-scripts start`);
+		runStream(`npx cross-env ${envString} react-scripts start`);
 	} catch (error) {
 		console.error("[bootstrap]", error);
 	}
 }
 
+// Handles ENV array and build a string to use
+function buildENV(env = []) {
+	if (env.length < 1) return "";
+
+	console.log("Building ENV to inject:");
+
+	// Build ENV string
+	let envString = "";
+	env.forEach((item, index) => {
+		if (index > 0) envString += ` `;
+		const envPair = `${item[0]}=${item[1]}`;
+		envString += envPair;
+		console.log("  ", index, envPair);
+	});
+
+	console.log("");
+
+	return envString;
+}
+
 // Execute OS commands, awaits response from stdout
 async function run(command) {
 	try {
-		const { stdout, stderr } = await exec(command);
+		const { stdout, stderr } = await execAwait(command);
 		return stdout?.trim();
 	} catch (e) {
 		console.error("[run]", e); // Should contain code (exit code) and signal (that caused the termination).
 	}
+}
+
+// Execute OS commands, awaits response from stdout
+function runStream(command) {
+	const process = exec(command);
+
+	process.stdout.on("data", (data) => {
+		console.log(data.toString());
+	});
+
+	process.stderr.on("data", (data) => {
+		console.error(data.toString());
+	});
+
+	process.on("exit", (code) => {
+		console.log(
+			"[runStream] Child process exited with code " + code.toString()
+		);
+	});
 }
