77babbaabfcc7b47544d3e8846c83e819b43ba62
hmerritt <harry@mrrtt.me>
2022-05-15 16:49:20 +0100

    rename all files to ts and tsx

--- src/index.tsx
+++ src/index.tsx
@@ -28,4 +28,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
+// @ts-ignore: config param is optional
 serviceWorkerRegistration.register();
