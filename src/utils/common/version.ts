96dd120a9ed76e8367fb8c1a98b98e5b662d7589
hmerritt <harry@mrrtt.me>
2022-06-09 18:33:34 +0100

    comment out node_env in version string

--- src/utils/common/version.ts
+++ src/utils/common/version.ts
@@ -2,6 +2,7 @@ export const appName = "App"; // Optionally use `process.env.REACT_APP_NAME`
 export const appVersion = process.env.REACT_APP_VERSION;
 export const gitBranch = process.env.REACT_APP_GIT_BRANCH;
 export const gitCommitHash = process.env.REACT_APP_GIT_COMMIT;
+export const environment = process.env.NODE_ENV;
 
 export const versionString = () => {
 	if (!appVersion) {
@@ -13,10 +14,17 @@ export const versionString = () => {
 	if (gitCommitHash) {
 		versionString += ` (`;
 
+		// ENV
+		// if (environment !== "production") {
+		// 	versionString += `${environment ?? "unknown"} `;
+		// }
+
+		// Branch name
 		if (gitBranch !== "master") {
 			versionString += `${gitBranch ?? "unknown"}/`;
 		}
 
+		// Commit hash
 		versionString += `${gitCommitHash})`;
 	}
 
