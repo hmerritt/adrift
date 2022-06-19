6882be47c1e894d055ebf26b9b1350480ffb0a24
hmerritt <harry@mrrtt.me>
2022-06-19 23:09:02 +0100

    setup initial theme structure

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,41 @@
+import mixins from "./mixins";
+
+// Import this into @linaria css`` to use colours, mixins & variables
+const theme = `${mixins}`;
+export default theme;
+
+// @TODO
+
+// Colors
+
+// Mixins
+
+// Chakra theme for reference
+//
+// const theme = extendTheme({
+// 	styles,
+// 	colors,
+// 	shadows,
+// 	// borders,
+// 	// Other foundational style overrides go here
+// 	components: {
+// 		Badge,
+// 		Button,
+// 		Heading,
+// 		Text,
+// 		Label,
+// 		Link,
+// 		Modal: {
+// 			baseStyle: {
+// 				dialogContainer: {
+// 					"@supports(height: -webkit-fill-available)": {}
+// 				}
+// 			}
+// 		}
+// 	},
+// 	sizes: {
+// 		container: {
+// 			xl: "86.25rem"
+// 		}
+// 	}
+// });
