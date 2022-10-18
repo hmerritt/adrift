1942145dbf45a6116eaa6f349dbb6e0a6ee0aa8a
hmerritt <harry@mrrtt.me>
2022-10-18 22:07:37 +0100

    test: vite test config

--- src/tests/render.tsx
+++ src/tests/render.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, JSXElementConstructor } from "react";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
-import { render as jestRender } from "@testing-library/react";
+import { render as reactRender } from "@testing-library/react";
 
 import store from "store";
 
@@ -20,7 +20,7 @@ export const render = (ui: element, route = "") => {
 		);
 	};
 
-	return jestRender(ui, { wrapper: Wrapper });
+	return reactRender(ui, { wrapper: Wrapper });
 };
 
 export const renderBasic = (ui: element) => {
@@ -28,5 +28,5 @@ export const renderBasic = (ui: element) => {
 		return <Provider store={store}>{children}</Provider>;
 	};
 
-	return jestRender(ui, { wrapper: Wrapper });
+	return reactRender(ui, { wrapper: Wrapper });
 };
--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -1,14 +1,5 @@
 // @ts-nocheck
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import { jestPreviewConfigure } from "jest-preview";
 import "@testing-library/jest-dom";
-import "styles/global/index.scss";
+import { injectGlobalLog, versionLog } from "utils";
 
-jestPreviewConfigure({
-	// Automatically preview the UI in the external browser when the test fails. You don't need to invoke preview.debug() by yourself anymore.
-	// Set to false if you experience any error or just want to opt-out.
-	autoPreview: true
-});
+injectGlobalLog();
