eae73369e7cbb93af16a40af6bc8362f113969ea
hmerritt <harry@mrrtt.me>
2023-03-17 13:33:39 +0000

    chore: add helpful comments explaining linaria styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -50,17 +50,18 @@ export const Home = () => {
 	);
 };
 
-// This will get compiled at build time into a css file
+// This will get compiled at build time into a css file.
+// Why? - Performance is *greatly* improved over something like styled-components which compiles at run time!
 const header = css`
-	${theme}
+	${theme} // Import theme object - can now use all SCSS variables and mixins set in styles/theme.ts
 	text-transform: uppercase;
 	font-size: 8rem;
 	font-weight: thin;
-	color: $red-500;
-	/* color: pink; */
+	color: $red-500; // See styles/colors.tsx
 	text-align: center;
-	text-shadow: $shadow-1;
+	text-shadow: $shadow-1; // See styles/shadows.tsx
 
+	// All valid SCSS syntax is valid here (this is just an example)
 	@for $i from 1 through 20 {
 		.stack.stack-#{$i} {
 			& > * {
