a7412728984a50df523328786b407ef7b9c60d40
hmerritt <harry@mrrtt.me>
2023-07-01 23:38:01 +0100

    feat: port useful Go strings functions

--- src/utils/common/strings.test.ts
+++ src/utils/common/strings.test.ts
@@ -1,5 +1,30 @@
 import { expect, test } from "vitest";
-import { padChar, padZeros, parseJSON } from "./strings";
+
+import {
+	hasPrefix,
+	hasSuffix,
+	padChar,
+	padZeros,
+	parseJSON,
+	trimPrefix,
+	trimSuffix
+} from "./strings";
+
+test("hasPrefix", () => {
+	expect(hasPrefix("Hello, World!", "Hello")).toBe(true);
+	expect(hasPrefix("ecdf0757-f748-4339-8200-6c09c", "ecdf0757-f748-43")).toBe(true);
+
+	expect(hasPrefix("Hello, World!", "World")).toBe(false);
+	expect(hasPrefix("ecdf0757-f748-4339-8200-6c09c", "cdf0757-f748-43")).toBe(false);
+});
+
+test("hasSuffix", () => {
+	expect(hasSuffix("Hello, World!", "World!")).toBe(true);
+	expect(hasSuffix("ecdf0757-f748-4339-8200-6c09c", "48-4339-8200-6c09c")).toBe(true);
+
+	expect(hasSuffix("Hello, World!", "Hello,")).toBe(false);
+	expect(hasSuffix("ecdf0757-f748-4339-8200-6c09c", "ecdf0757-f7")).toBe(false);
+});
 
 test("padChar", () => {
 	expect(padChar("", 5, "!")).toBe("!!!!!");
@@ -27,4 +52,20 @@ test("parseJSON", () => {
 
 	expect(parseJSON(`{"key:not valid json"}`)).toBe(undefined);
 	expect(parseJSON("[wow")).toBe(undefined);
+});
+
+test("trimPrefix", () => {
+	expect(trimPrefix("Hello World", "Hello W")).toBe("orld");
+	expect(trimPrefix("!@#wow#(*&#/", "!@#w")).toBe("ow#(*&#/");
+
+	expect(trimPrefix("Hello World", " World")).toBe("Hello World");
+	expect(trimPrefix("!@#wow#(*&#/", "@#wo")).toBe("!@#wow#(*&#/");
+});
+
+test("trimSuffix", () => {
+	expect(trimSuffix("Hello World", "o World")).toBe("Hell");
+	expect(trimSuffix("!@#wow#(*&#/", "w#(*&#/")).toBe("!@#wo");
+
+	expect(trimSuffix("Hello World", " Hello ")).toBe("Hello World");
+	expect(trimSuffix("!@#wow#(*&#/", "(*&#")).toBe("!@#wow#(*&#/");
 });
--- src/utils/common/strings.ts
+++ src/utils/common/strings.ts
@@ -1,4 +1,22 @@
 /**
+ * Tests whether the string `s` begins with `prefix`.
+ *
+ * Ported from Go-lang `strings.HasPrefix`
+ */
+export const hasPrefix = (s: string, prefix: string) => {
+	return s.length >= prefix.length && s.substring(0, prefix.length) === prefix;
+};
+
+/**
+ * Tests whether the string `s` ends with `suffix`.
+ *
+ * Ported from Go-lang `strings.HasSuffix`
+ */
+export const hasSuffix = (s: string, suffix: string) => {
+	return s.length >= suffix.length && s.slice(-suffix.length) === suffix;
+};
+
+/**
  * Use char(s) to pad an input string/number to a certain length.
  */
 export const padChar = (
@@ -31,4 +49,28 @@ export const parseJSON = (value: string | null): any | undefined => {
 		log("warn", value);
 		return undefined;
 	}
+};
+
+/**
+ * Returns `s` without the provided leading `prefix` string.
+ *
+ * If `s` doesn't start with `prefix`, `s` is returned unchanged.
+ *
+ * Ported from Go-lang `strings.TrimPrefix`
+ */
+export const trimPrefix = (s: string, prefix: string) => {
+	if (hasPrefix(s, prefix)) return s.slice(prefix.length);
+	return s;
+};
+
+/**
+ * Returns `s` without the provided trailing `suffix` string.
+ *
+ * If `s` doesn't start with `suffix`, `s` is returned unchanged.
+ *
+ * Ported from Go-lang `strings.TrimSuffix`
+ */
+export const trimSuffix = (s: string, suffix: string) => {
+	if (hasSuffix(s, suffix)) return s.slice(0, s.length - suffix.length);
+	return s;
 };
