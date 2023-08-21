6e86a92631305e5d1d0c3e26efd52dc99594324b
hmerritt <harry@mrrtt.me>
2023-08-21 21:21:12 +0100

    refactor: getNumberOfEventListeners

--- src/lib/global/devTools.ts
+++ src/lib/global/devTools.ts
@@ -4,11 +4,12 @@ export const getNumberOfEventListeners = () => {
 	try {
 		if (typeof window === "undefined" || !(window as any)?.getEventListeners)
 			return -1;
-		return Array.from(document.querySelectorAll("*")).reduce(function (pre, dom) {
-			const clks = (window as any).getEventListeners(dom)?.click;
-			pre += clks ? clks.length || 0 : 0;
-			return pre;
-		}, 0);
+		const events = (window as any).getEventListeners(window);
+		let pre = 0;
+		Object.keys(events).forEach(function (evt) {
+			pre += events?.[evt]?.length || 0;
+		});
+		return pre;
 	} catch (e) {
 		return -1;
 	}
@@ -18,20 +19,20 @@ export const getObjectOfEventListeners = () => {
 	try {
 		if (typeof window === "undefined" || !(window as any)?.getEventListeners)
 			return {};
-		return Array.from(document.querySelectorAll("*")).reduce(function (
-			pre: any,
-			dom: any
-		) {
-			const evtObj = (window as any).getEventListeners(dom);
-			Object.keys(evtObj).forEach(function (evt) {
-				if (typeof pre[evt] === "undefined") {
-					pre[evt] = 0;
-				}
-				pre[evt] += evtObj[evt].length;
-			});
-			return pre;
-		},
-		{});
+		return Array.from(document.querySelectorAll("*")).reduce(
+			function (pre: any, dom: any) {
+				const evtObj = (window as any).getEventListeners(dom);
+				Object.keys(evtObj).forEach(function (evt) {
+					if (typeof pre[evt] === "undefined") {
+						pre[evt] = 0;
+					}
+					pre[evt] += evtObj[evt].length;
+					pre["_total"] += evtObj[evt].length;
+				});
+				return pre;
+			},
+			{ _total: 0 }
+		);
 	} catch (e) {
 		return {};
 	}
