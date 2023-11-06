cef6ed4dbbbcc66b99857fa5a0317a59948cbab1
hmerritt <harry@mrrtt.me>
2023-11-06 22:07:39 +0000

    feat: Image fallback and loading src

--- src/view/components/Image/index.tsx
+++ src/view/components/Image/index.tsx
@@ -1,15 +1,25 @@
 import { css, cx } from "@linaria/core";
+import { useState } from "react";
 
 export type ImageProps = JSX.IntrinsicElements["img"] & {
-	/** Maintains aspect ratio - even when render width/height are fluid */
+	/** Maintains image aspect ratio - even when render width/height are fluid */
 	aspectRatioMaintain?: boolean;
+	/** Image source that is shown if `src` image fails to load (also shown while loading if `loadingSrc` is unset) */
+	fallbackSrc?: string;
+	/** Image source that is shown while `src` image is loading */
+	loadingSrc?: string;
 };
 
 export const Image = ({
-	className,
 	aspectRatioMaintain,
+	className,
+	fallbackSrc,
 	height,
+	loadingSrc,
+	src,
 	width,
+	onError,
+	onLoad,
 	...props
 }: ImageProps) => {
 	if (aspectRatioMaintain && (width == null || height == null)) {
@@ -20,17 +30,59 @@ export const Image = ({
 		);
 	}
 
+	const usesFallback = !!fallbackSrc || !!loadingSrc;
+
+	const [{ isLoading, hasError }, setState] = useState({
+		isLoading: true,
+		hasError: false
+	});
+
 	return (
-		<img
-			className={cx(className, aspectRatioMaintain && arm)}
-			draggable={false} // <- All websites should do this my my
-			width={width}
-			height={height}
-			{...props}
-			style={{
-				...props.style
-			}}
-		/>
+		<>
+			<img
+				src={src}
+				className={cx(
+					className,
+					aspectRatioMaintain && arm,
+					usesFallback && (isLoading || hasError) && "hidden"
+				)}
+				draggable={false} // <- All websites should do this my my
+				width={width}
+				height={height}
+				{...((onLoad || usesFallback) && {
+					onLoad: (e) => {
+						if (onLoad) onLoad(e);
+						setState((prev) => ({ ...prev, isLoading: false }));
+					}
+				})}
+				{...((onError || usesFallback) && {
+					onError: (e) => {
+						if (onError) onError(e);
+						setState((prev) => ({
+							...prev,
+							isLoading: false,
+							hasError: true
+						}));
+					}
+				})}
+				{...props}
+			/>
+
+			{usesFallback && (
+				<img
+					src={isLoading && loadingSrc ? loadingSrc : fallbackSrc}
+					className={cx(
+						className,
+						aspectRatioMaintain && arm,
+						!isLoading && !hasError && "hidden"
+					)}
+					draggable={false}
+					width={width}
+					height={height}
+					{...props}
+				/>
+			)}
+		</>
 	);
 };
 
