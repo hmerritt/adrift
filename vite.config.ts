4c9f47303ad0fddcf2085f22a6d451b80ae22975
hmerritt <harry@mrrtt.me>
2024-01-31 15:59:33 +0000

    fix: vitest exclude e2e directory

--- vite.config.ts
+++ vite.config.ts
@@ -47,13 +47,14 @@ export default defineConfig({
 		environment: "happy-dom",
 		setupFiles: "./src/tests/setupTests.ts",
 		css: true, // @Note Parsing CSS is slow
+		exclude: ["node_modules", "tests-e2e", "dist", ".idea", ".git", ".cache"],
 		coverage: {
 			enabled: false,
 			provider: "v8"
 		},
 		benchmark: {
 			include: ["**/*.{bench,benchmark}.?(c|m)[jt]s?(x)"],
-			exclude: ["node_modules", "dist", ".idea", ".git", ".cache"]
+			exclude: ["node_modules", "tests-e2e", "dist", ".idea", ".git", ".cache"]
 		},
 		// Debug
 		logHeapUsage: true
