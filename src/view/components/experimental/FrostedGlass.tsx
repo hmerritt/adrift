4b8ad1ac984d5e69bc743888121ad6a62ef474bb
hmerritt <harry@mrrtt.me>
2024-02-10 00:29:48 +0000

    feat: FrostedGlass direction row or column

--- src/view/components/experimental/FrostedGlass.tsx
+++ src/view/components/experimental/FrostedGlass.tsx
@@ -1,10 +1,14 @@
-import { css } from "@linaria/core";
+import { css, cx } from "@linaria/core";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 export type FrostedGlassProps = JSX.IntrinsicElements["div"] & {
-	/** Target maximum width of each pane */
+	/** Pane direction */
+	paneDirection?: "row" | "column";
+	/** Target maximum width of each pane (only used when `paneDirection` is set to `row`) */
 	paneMaxWidth?: number;
-	/** Initial number of panes - before first render we need to show some panes */
+	/** Target maximum height of each pane (only used when `paneDirection` is set to `column`) */
+	paneMaxHeight?: number;
+	/** Initial number of panes shown before first render (pane count is re-calculated after component mounts) */
 	paneInitialCount?: number;
 	/** Recalculate pane count on window resize (responsive, but may impact performance) */
 	reactToWindowResize?: boolean;
@@ -18,7 +22,9 @@ export type FrostedGlassProps = JSX.IntrinsicElements["div"] & {
 export const FrostedGlass: React.FC<FrostedGlassProps> = ({
 	children,
 	className,
+	paneDirection = "row",
 	paneMaxWidth = 25,
+	paneMaxHeight = 25,
 	paneInitialCount = 5,
 	reactToWindowResize = false,
 	...divProps
@@ -29,11 +35,18 @@ export const FrostedGlass: React.FC<FrostedGlassProps> = ({
 
 	const calculatePaneCount = useCallback(() => {
 		if (!$div.current) return;
-		setPaneCount(
-			Math.floor(
+
+		if (paneDirection === "row") {
+			setPaneCount(
 				Math.floor($div.current?.clientWidth / paneMaxWidth) || paneInitialCount
-			)
-		);
+			);
+		}
+
+		if (paneDirection === "column") {
+			setPaneCount(
+				Math.floor($div.current?.clientHeight / paneMaxHeight) || paneInitialCount
+			);
+		}
 	}, [paneInitialCount, paneMaxWidth]);
 
 	useEffect(() => {
@@ -52,9 +65,14 @@ export const FrostedGlass: React.FC<FrostedGlassProps> = ({
 		<div ref={$div} {...divProps} className={className}>
 			{children}
 
-			<div className={paneContainer}>
+			<div
+				className={cx(paneContainer, paneDirection === "row" ? "row" : "column")}
+			>
 				{[...Array(paneCount).keys()].map((i) => (
-					<div key={i} className={pane} />
+					<div
+						key={i}
+						className={cx(pane, paneDirection === "row" ? "row" : "column")}
+					/>
 				))}
 			</div>
 		</div>
@@ -66,19 +84,33 @@ const paneContainer = css`
 	z-index: -1;
 	inset: 0;
 	display: flex;
-	flex-direction: row;
 	align-items: stretch;
 	width: 100%;
 	height: 100%;
+
+	&.row {
+		flex-direction: row;
+	}
+
+	&.column {
+		flex-direction: column;
+	}
 `;
 
 const pane = css`
 	flex: 1;
-	height: 100%;
 	background: linear-gradient(
 		to right,
 		rgba(255, 255, 255, 0.2),
 		rgba(255, 255, 255, 0.1)
 	);
 	backdrop-filter: blur(10px);
+
+	&.row {
+		height: 100%;
+	}
+
+	&.column {
+		width: 100%;
+	}
 `;
