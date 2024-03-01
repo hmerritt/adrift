c59e7b8b3deb42a95b1d9997f7117905d270e31a
hmerritt <harry@mrrtt.me>
2024-03-01 18:58:10 +0000

    chore: bump dependency versions

--- scripts/bootstrap/version.cjs
+++ scripts/bootstrap/version.cjs
@@ -3,7 +3,7 @@
 /**
  * Internal adrift version.
  */
-const adriftVersion = "0.10.441";
+const adriftVersion = "0.10.442";
 
 /**
  * Checks with latest GitHub release to see if there is an update.
