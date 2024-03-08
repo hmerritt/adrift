ba2fc0f88cc94d12ae36a5d9f0b778f6818cffd3
hmerritt <harry@mrrtt.me>
2024-03-08 20:55:05 +0000

    fix: remove redux providers

--- scripts/bootstrap/version.cjs
+++ scripts/bootstrap/version.cjs
@@ -3,7 +3,7 @@
 /**
  * Internal adrift version.
  */
-const adriftVersion = "0.10.449";
+const adriftVersion = "0.10.450";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
