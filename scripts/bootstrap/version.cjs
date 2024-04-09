3d1bbfdc8b8eb784008ec75b8229c871a1479091
hmerritt <harry@mrrtt.me>
2024-04-09 22:25:04 +0100

    chore: bump deps

--- scripts/bootstrap/version.cjs
+++ scripts/bootstrap/version.cjs
@@ -3,7 +3,7 @@
 /**
  * Internal adrift version.
  */
-const adriftVersion = "0.11.465";
+const adriftVersion = "0.11.468";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
