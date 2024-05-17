437dcca1cfcf0bd6807b40b53c0108bacbeb39f0
hmerritt <harry@mrrtt.me>
2024-05-17 12:45:23 +0100

    wip: scss with linaria/atomic

--- src/view/routes/user.$userId.lazy.tsx
+++ src/view/routes/user.$userId.lazy.tsx
@@ -1,3 +1,5 @@
+import { css } from "@linaria/atomic";
+import { cx } from "@linaria/core";
 import { Link, createLazyFileRoute } from "@tanstack/react-router";
 
 import { Stack } from "view/components";
@@ -13,6 +15,19 @@ function UserRoute() {
 		<Stack spacing={6}>
 			<h1>User {userId}</h1>
 			<Link to="/">Back</Link>
+
+			<p className={cx(textTmp)}>
+				{" "}
+				position: relative; width: 700px; height: 350px; margin: auto; display:
+				flex; overflow: hidden; align-items: center; flex-direction: column;
+				justify-content: center; box-shadow: shadowBlock($blue-400);
+			</p>
 		</Stack>
 	);
 }
+
+const textTmp = css`
+	$purple-900: #322659;
+	color: $purple-900;
+	width: 300px;
+`;
