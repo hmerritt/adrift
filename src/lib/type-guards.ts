441313bf7c391d9cd33ac213b04c49244fd2369d
hmerritt <harry@mrrtt.me>
2024-05-17 12:52:02 +0100

    feat: arraySpread to conditionally spread an item into an array

--- src/lib/type-guards.ts
+++ src/lib/type-guards.ts
@@ -8,3 +8,21 @@ export const hasProp = <K extends PropertyKey>(
 ): data is Record<K, unknown> => {
 	return prop in data;
 };
+
+/**
+ * Helper to conditionally spread an item into an array.
+ */
+export const arraySpread = <TItem>(
+	/** Conditional value. When truthy, `itemToSpread` is returned. */
+	isTruthy: any,
+	/** Item that gets returned when conditional is truthy */
+	itemToSpread: TItem | TItem[],
+	/** Option to spread multiple values when `itemToSpread` is an array */
+	spreadMultiple = false
+) => {
+	if (isTruthy)
+		return spreadMultiple && Array.isArray(itemToSpread)
+			? (itemToSpread as TItem[])
+			: [itemToSpread as TItem];
+	return [] as TItem[];
+};
