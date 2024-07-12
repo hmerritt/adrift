ada63f904f479ff99d98c4142d2a344a2e4c8467
hmerritt <harry@mrrtt.me>
2024-07-12 16:28:57 +0100

    feat: isTruthy helper

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -7,7 +7,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.11.510";
+export const adriftVersion = "0.11.512";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
