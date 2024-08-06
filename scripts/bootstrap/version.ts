cf7030f9e16e435e1ef1002f085970c144ad3c67
hmerritt <harry@mrrtt.me>
2024-08-06 16:45:20 +0100

    feat: yarn 4.4.0

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -7,7 +7,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.11.519";
+export const adriftVersion = "0.11.520";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
