a05bbe2e7ae294e61f099cffa0b252fdd18db747
hmerritt <harry@mrrtt.me>
2024-10-09 16:30:07 +0100

    fix: yarn.lock

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -7,7 +7,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.11.539";
+export const adriftVersion = "0.11.540";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
