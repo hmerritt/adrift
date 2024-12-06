7862c237af99a6fe11285efd7036b6a5f434bfab
hmerritt <harry@mrrtt.me>
2024-12-06 14:15:38 +0000

    chore: bump deps
    React 19 🎉

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -8,7 +8,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.11.589";
+export const adriftVersion = "0.11.590";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
