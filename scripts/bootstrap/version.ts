c3dcb7441254e597a9a78afe887c57d9a337b74a
hmerritt <harry@mrrtt.me>
2025-04-13 20:19:08 +0100

    fix: namespace log for parseJSON try-catch

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -8,7 +8,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.11.639";
+export const adriftVersion = "0.11.641";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
