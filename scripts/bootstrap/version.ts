d990de50e559aec312054023651050483ae595d9
hmerritt <harry@mrrtt.me>
2025-04-15 22:22:44 +0100

    test: basic test for Image

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -8,7 +8,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.11.645";
+export const adriftVersion = "0.11.650";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
