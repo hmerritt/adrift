7621ab7678388023d8c2d6d47403c13d90db91e5
hmerritt <harry@mrrtt.me>
2025-06-16 11:11:11 +0100

    fix: revert stylex

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -8,7 +8,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.11.656";
+export const adriftVersion = "0.11.657";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
