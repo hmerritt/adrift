33b75035821afda71ce26086f45017301aebacec
hmerritt <harry@mrrtt.me>
2025-10-08 11:31:03 +0100

    feat: add useful react-cosmos plugins

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -8,7 +8,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.12.712";
+export const adriftVersion = "0.12.713";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
