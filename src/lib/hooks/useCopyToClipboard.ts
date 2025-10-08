ad8f9bab7767350e8183318143861d1603436a57
hmerritt <harry@mrrtt.me>
2025-10-08 13:13:54 +0100

    feat: refactor useCopyToClipboard

--- src/lib/hooks/useCopyToClipboard.ts
+++ src/lib/hooks/useCopyToClipboard.ts
@@ -4,29 +4,41 @@ type CopiedValue = string | null;
 type CopyFn = (text: string) => Promise<boolean>; // Return success
 
 /**
- * Provides a copy method to save a string in the clipboard and the copied value (default: null).
- * If anything doesn't work, it prints a warning in the console and the value will be null.
+ * Copies text to clipboard.
+ *
+ * Returns function to copy, and the copied text (or null if nothing copied).
  */
-export function useCopyToClipboard(): [CopiedValue, CopyFn] {
+export function useCopyToClipboard(): [CopyFn, CopiedValue] {
 	const [copiedText, setCopiedText] = useState<CopiedValue>(null);
 
 	const copy: CopyFn = async (text) => {
-		if (!navigator?.clipboard) {
-			logn.warn("useCopyToClipboard", "Clipboard not supported");
-			return false;
-		}
-
-		// Try to save to clipboard then save it in the state if worked
-		try {
-			await navigator.clipboard.writeText(text);
-			setCopiedText(text);
-			return true;
-		} catch (error) {
-			logn.warn("useCopyToClipboard", "Copy failed", error);
-			setCopiedText(null);
-			return false;
-		}
+		const copied = await copyToClipboard(text);
+		if (copied) setCopiedText(text);
+		else setCopiedText(null);
+		return copied;
 	};
 
-	return [copiedText, copy];
+	return [copy, copiedText];
 }
+
+/**
+ * Copies text to clipboard.
+ */
+export const copyToClipboard = async (text: string): Promise<boolean> => {
+	if (!navigator?.clipboard) {
+		logn.error("copyToClipboard", "Clipboard not supported");
+		return false;
+	}
+
+	const [, error] = await go(async () => {
+		await navigator.clipboard.writeText(text);
+		return true;
+	});
+
+	if (error) {
+		logn.error("copyToClipboard", error);
+		return false;
+	}
+
+	return true;
+};
