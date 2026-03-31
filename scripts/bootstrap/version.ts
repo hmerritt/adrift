fdd20414c4e47cb45c2206ce1de61b226eaa8e20
hmerritt <harry@mrrtt.me>
2026-03-31 17:43:04 +0100

    chore: bump deps

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -6,7 +6,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.14.841";
+export const adriftVersion = "0.14.842";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
