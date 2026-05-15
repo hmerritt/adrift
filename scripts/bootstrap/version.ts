362459fe0fc20998edd19760d8d80e2bbf8e48dd
hmerritt <harry@mrrtt.me>
2026-05-15 14:44:42 +0800

    chore: set minimumReleaseAge to bun for package security

--- scripts/bootstrap/version.ts
+++ scripts/bootstrap/version.ts
@@ -6,7 +6,7 @@ import * as core from "./core";
 /**
  * Internal adrift version.
  */
-export const adriftVersion = "0.14.844";
+export const adriftVersion = "0.14.845";
 
 /**
  * Bumps the adrift `patch` version number using the total commit count.
